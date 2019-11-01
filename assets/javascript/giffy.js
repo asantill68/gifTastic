a6c609d9c50a671a217bc76de84e0543876d04bf
Alex Santillan <asantill68@yahoo.com>
2019-11-01 00:27:21 -0500

    Need to fix Botton-add and Style

--- assets/javascript/giffy.js
+++ assets/javascript/giffy.js
@@ -1,9 +1,9 @@
 $(function(){
     populateButtons(searchArray,'searchButton', '#buttonsArea');
-    console.log('Page Loaded.');
+    //console.log('Page Loaded.');
 });
 
-var searchArray = ['Dog', 'Cat', 'Bird'];
+var searchArray = ['Long Jump', 'Snow Board', 'Curling'];
 
 function populateButtons(searchArray,classToAdd,areaToAddTo){
 //Clear the area so duplicate buttons are not added
@@ -24,6 +24,40 @@ $(document).on('click', '.searchButton', function(){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        //console.log(response);
+        for(i=0; i<response.data.length; i++){
+            var searchDiv = $('<div class="search-tiem">');
+            var rating    = response.data[i].rating;
+            var p         = $('<p>').text('Rating: ' + rating);
+            var animated  = response.data[i].images.fixed_height.url;
+            var still     = response.data[i].images.fixed_height_still.url;
+            var image     = $('<img>');
+            image.attr('src', still);
+            image.attr('data-still', still);
+            image.attr('data-animated', animated);
+            image.attr('data-state', 'still');
+            image.addClass('searchImage');
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $('#searches').append(searchDiv);
+        }
     })  
+})
+
+$(document).on('click', '.searchImage', function(){
+    var state = $(this).attr('data-state');
+    if (state=='still'){
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+})
+
+$('#addSearch').on('click', function(){
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
+    return false;
 })
